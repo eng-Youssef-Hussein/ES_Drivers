dc8cd1f6be0388ad1b7db567cb0277480f8ef618
Youssef Hussein <youssefrtsdfg@gmail.com>
2026-02-27 11:00:43 +0200

    Add files via upload

---
+++ ES WorkShop/STM32F103C8/LIB/STD_TYPES.h
@@ -0,0 +1,36 @@
+/*********************************************************************************/
+/****************   Auther    : Youssef Hussein              *********************/
+/****************   Date      : 27 Feb 2026                  *********************/
+/****************   Version.  : 0.1                          *********************/
+/****************   File Name : STD_TYPES.h                  *********************/
+/*********************************************************************************/
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
+typedef unsigned char               u8;
+typedef unsigned short              u16;
+typedef unsigned int                u32;
+typedef unsigned long               u64;
+
+typedef signed char                 s8;
+typedef signed short                s16;
+typedef signed int                  s32;
+typedef signed long                 s64;
+
+typedef float                       f32;
+typedef double                      f64;
+typedef long double                 f96;
+
+
+
+
+
+
+
+
+
+
+#endif // STD_TYPES_H_
+
+
